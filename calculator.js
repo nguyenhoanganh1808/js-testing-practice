54c12a2ba998bfd4e0a35f2b2cd79ba8743b8a7a
nguyenhoanganh1808 <1808nguyenhoanganh2003@gmail.com>
2024-09-20 21:34:19 +0700

    add calculator

--- calculator.js
+++ calculator.js
@@ -1,1 +1,14 @@
-export default function calculator() {}
+export let calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  subtract: function (a, b) {
+    return a - b;
+  },
+  divide: function (a, b) {
+    return a / b;
+  },
+  multiply: function (a, b) {
+    return a * b;
+  },
+};
--- calculator.test.js
+++ calculator.test.js
@@ -1,4 +1,4 @@
-import calculator from "./calculator";
+import { calculator } from "./calculator";
 
 test("should exist", () => {
   expect(calculator).not.toEqual({});
@@ -9,4 +9,66 @@ test("should have four operation: add, subtract, divide, and multiply", () => {
   expect(calculator.subtract).toBeDefined();
   expect(calculator.divide).toBeDefined();
   expect(calculator.multiply).toBeDefined();
+});
+
+test("should take two numbers and return the correct calculation", () => {
+  const addTestCases = [
+    {
+      input: [1, 2],
+      expect: 3,
+    },
+    {
+      input: [2, 100.2],
+      expect: 102.2,
+    },
+  ];
+
+  addTestCases.forEach((test) => {
+    expect(calculator.add(...test.input)).toBeCloseTo(test.expect);
+  });
+
+  const subtractTestCase = [
+    {
+      input: [1, 2],
+      expect: -1,
+    },
+    {
+      input: [2, 100.2],
+      expect: -98.2,
+    },
+  ];
+
+  subtractTestCase.forEach((test) => {
+    expect(calculator.subtract(...test.input)).toBeCloseTo(test.expect);
+  });
+
+  const multiplyTestCase = [
+    {
+      input: [3, 2],
+      expect: 6,
+    },
+    {
+      input: [2, 100.2],
+      expect: 200.4,
+    },
+  ];
+
+  multiplyTestCase.forEach((test) => {
+    expect(calculator.multiply(...test.input)).toBeCloseTo(test.expect);
+  });
+
+  const divideTestCase = [
+    {
+      input: [3, 2],
+      expect: 1.5,
+    },
+    {
+      input: [2, 100.2],
+      expect: 0.01996007984,
+    },
+  ];
+
+  divideTestCase.forEach((test) => {
+    expect(calculator.divide(...test.input)).toBeCloseTo(test.expect);
+  });
 });
